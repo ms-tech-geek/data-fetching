845256ba6aedcbd806e561a35b67faa6cba2d847
ms-tech-geek <thecorporator@gmail.com>
2024-08-16 13:37:26 +0530

    completed fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
-import { useRef, useState, useCallback } from 'react';
+import { useRef, useState, useCallback, useEffect } from 'react';
 
 import Places from './components/Places.jsx';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
-import { updateUserPlaces } from './http.js';
+import { fetchUserPlaces, updateUserPlaces } from './http.js';
 import Error from './components/Error.jsx';
 
 function App() {
   const selectedPlace = useRef();
 
   const [userPlaces, setUserPlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
   const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState();
   const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+      try {
+        const places = await fetchUserPlaces();
+        setUserPlaces(places)
+      }
+      catch (error) {
+        setError({message: error.message || 'Failed to fetch user places.'})
+        setIsFetching(false);
+      }
+      setIsFetching(false);
+    }
+    fetchPlaces();
+  }, [])
 
   function handleStartRemovePlace(place) {
     setModalIsOpen(true);
@@ -48,14 +66,14 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
     );
 
-    try{
-  updateUserPlaces(userPlaces.filter(place => place.id !== selectedPlace.current.id))
-}
-catch(error){
-  setUserPlaces(userPlaces);
-  setErrorUpdatingPlaces({message: error.message || 'Failed to delete place.'})
-}
-  
+    try {
+      updateUserPlaces(userPlaces.filter(place => place.id !== selectedPlace.current.id))
+    }
+    catch (error) {
+      setUserPlaces(userPlaces);
+      setErrorUpdatingPlaces({ message: error.message || 'Failed to delete place.' })
+    }
+
 
     setModalIsOpen(false);
   }, []);
@@ -86,12 +104,15 @@ catch(error){
         </p>
       </header>
       <main>
-        <Places
+        {error && <Error title="An error ocurred!" message={error.message}/>}
+        {!error && <Places
           title="I'd like to visit ..."
           fallbackText="Select the places you would like to visit below."
+          isLoading={isFetching}
+          loadingText="Fetching your places..."
           places={userPlaces}
           onSelectPlace={handleStartRemovePlace}
-        />
+        />}
 
         <AvailablePlaces onSelectPlace={handleSelectPlace} />
       </main>
